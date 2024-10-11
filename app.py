b8008b7399d4b23e7819e9c514f43c0f8db986b7
ShandanPy <shandan.spencer@icloud.com>
2024-10-11 12:52:12 -0500

    first Commit

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return render_template('base.html')
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
